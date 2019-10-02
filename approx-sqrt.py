190d4c719d8407b2c66b9b02351b73bf6baf9ca4
Liam-Higgins2018 <liammhiggins@hotmail.com>
2019-10-02 16:35:11 +0100

    Approx Square Root

---
+++ approx-sqrt.py
@@ -0,0 +1,14 @@
+def sqrt(x):
+
+    #Intital guess
+    z = 1.0
+    #Keep getting a better estimate for the square root of x,
+    #until you are within two decimal places
+    while abs(z*z - x) > 0.01:
+    #Get a better aproximation for the sqr root
+    z -= (z*z -x) / (2^z)
+    #Returns z
+    return z
+
+    #Get the sqr rt of 8 and print it
+    print(sqrt(8.0))
